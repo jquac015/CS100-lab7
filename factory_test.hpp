98fcdb60c614ef165e8874211d0880922aa647f3
Jackie-Qin <jackieqin221@gmail.com>
2020-11-30 21:08:11 -0800

    Update factory and test

--- factory_test.hpp
+++ factory_test.hpp
@@ -1,6 +1,34 @@
 #ifndef __FACTORY_TEST_HPP_
 #define __FACTORY_TEST_HPP_
 
+#include "gtest/gtest.h"
+
 #include "factory.hpp"
+
+TEST(FactoryTest, EvaluateSimpleAdd) {
+    char** p = new char*[3];
+    p[0] = "./calculator";
+    p[1] = "-9";
+    p[2] = "+";
+    p[3] = "10";
+    factory* f = new factory();
+    Base* test = f->parse(p, 4);
+    EXPECT_EQ(test->evaluate(), 1);
+    EXPECT_EQ(test->stringify(), "(-9.000000)+10.000000");
+}
+
+TEST(FactoryTest, EvaluateSimpleMinus) {
+    char** p = new char*[3];
+    p[0] = "./calculator";
+    p[1] = "10";
+    p[2] = "-";
+    p[3] = "9";
+    factory* f = new factory();
+    Base* test = f->parse(p, 4);
+    EXPECT_EQ(test->evaluate(), 1);
+    EXPECT_EQ(test->stringify(), "10.000000-9.000000");
+}
+
+
 
 #endif
